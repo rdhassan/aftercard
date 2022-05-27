20cfc5d105e1c007d3d17952cdb84c7e06e1559f
rdhassan <dawood.hassan@phaedrasolutoins.com>
2022-05-27 19:57:41 +0500

    sidebar set on mobile

--- src/component/sidebar/index.jsx
+++ src/component/sidebar/index.jsx
@@ -18,45 +18,50 @@ const SideBar = () => {
   }
 
   return (
-    <div expand="md" className='py-5 sidebar'>
-      <div className='top'>
-        <Navbar.Brand href="#home" className='me-0'>
-          <img src={logo} width="206" alt="no-logo" />
-        </Navbar.Brand>
+    <>
+      <label className='toggle-sidebar d-flex m-0'>
+        <input type="checkbox" />
+        <div expand="md" className='py-5 sidebar flex-1'>
+          <div className='top'>
+            <Navbar.Brand href="#home" className='me-0'>
+              <img src={logo} width="206" alt="no-logo" />
+            </Navbar.Brand>
 
-        <ListGroup className='menu'>
-          <ListGroup.Item action href="#link1">
-            <img src={home} alt="no-logo" /> Dashboard
-          </ListGroup.Item>
-          <ListGroup.Item action href="#link2">
-            <img src={list} alt="no-logo" /> Claims
-          </ListGroup.Item>
-          <ListGroup.Item action href="#link2">
-            <img src={FileBlack} alt="no-logo" /> Merchants
-          </ListGroup.Item>
-          <ListGroup.Item action href="#link2">
-            <img src={Team} alt="no-logo" /> Team
-          </ListGroup.Item>
-          <ListGroup.Item action href="#link2">
-            <img src={Transition} alt="no-logo" /> Transactions
-          </ListGroup.Item>
-          <ListGroup.Item action href="#link3">
-            <img src={setting} alt="no-logo" /> Settings
-          </ListGroup.Item>
-        </ListGroup>
-      </div>
-      <div className='bottom'>
-        <ListGroup className='menu'>
-          <div className='support mb-3'>
-            <img src={comment2x} alt="no-logo" />
-            <a href='#' className='sup-link text-decoration-none text-center'>Support</a>
+            <ListGroup className='menu'>
+              <ListGroup.Item action href="#link1">
+                <img src={home} alt="no-logo" /> Dashboard
+              </ListGroup.Item>
+              <ListGroup.Item action href="#link2">
+                <img src={list} alt="no-logo" /> Claims
+              </ListGroup.Item>
+              <ListGroup.Item action href="#link2">
+                <img src={FileBlack} alt="no-logo" /> Merchants
+              </ListGroup.Item>
+              <ListGroup.Item action href="#link2">
+                <img src={Team} alt="no-logo" /> Team
+              </ListGroup.Item>
+              <ListGroup.Item action href="#link2">
+                <img src={Transition} alt="no-logo" /> Transactions
+              </ListGroup.Item>
+              <ListGroup.Item action href="#link3">
+                <img src={setting} alt="no-logo" /> Settings
+              </ListGroup.Item>
+            </ListGroup>
           </div>
-          <ListGroup.Item onClick={logoutHandler} action href="#a">
-            <img src={logout} alt="no-logo" /> Log-Out
-          </ListGroup.Item>
-        </ListGroup>
-      </div>
-    </div>
+          <div className='bottom'>
+            <ListGroup className='menu'>
+              <div className='support mb-3'>
+                <img src={comment2x} alt="no-logo" />
+                <a href='#' className='sup-link text-decoration-none text-center'>Support</a>
+              </div>
+              <ListGroup.Item onClick={logoutHandler} action href="#a">
+                <img src={logout} alt="no-logo" /> Log-Out
+              </ListGroup.Item>
+            </ListGroup>
+          </div>
+        </div>
+      </label>
+    </>
   )
 }
 
