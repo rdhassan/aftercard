a922dbcb13abb4dfd85b6321c46261831de7c2e5
rdhassan <dawood.hassan@phaedrasolutoins.com>
2022-06-13 14:49:10 +0500

    minor changes

--- src/component/dropdown/index.jsx
+++ src/component/dropdown/index.jsx
@@ -1,18 +1,19 @@
+import React, {useState} from "react";
 import { InputGroup, DropdownButton, Dropdown } from 'react-bootstrap';
 import './index.scss';
 const DropdownSelect = ({title, items=[]}) => {
+  const [selectedItem,setSelectedItem] = useState('');
 
   return (
       <InputGroup className="select-dropdown">
         <DropdownButton
           variant="outline-secondary"
-          title={title}
+          title={selectedItem || title}
+          onSelect={(eventKey)=>{setSelectedItem(eventKey)}}
         >
-          {items.map((item, index)=>(<Dropdown.Item key={index} href="#">{item}</Dropdown.Item>))}
-          {/* <Dropdown.Item href="#"></Dropdown.Item>
-          <Dropdown.Item href="#"></Dropdown.Item>
-          <Dropdown.Item href="#"></Dropdown.Item>
-          <Dropdown.Item href="#"> Issue</Dropdown.Item> */}
+          {items.map((item, index)=>(
+          <Dropdown.Item key={index} href="#" eventKey={item} >{item}</Dropdown.Item>
+          ))}
         </DropdownButton>
       </InputGroup>
   )
